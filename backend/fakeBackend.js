8febe204143cebe211e3e7d9b0ef890dd833c07f
Matthias Dittgen <matthias.dittgen@valiton.com>
2016-04-23 16:48:01 +0200

    static data test for cors

--- backend/fakeBackend.js
+++ backend/fakeBackend.js
@@ -1,11 +1,132 @@
 var express = require('express');
 var app = express();
 
-app.get('/tasks', function(req, res){
-    var o = {
-        avatar: "cdsvs.png",
-        test: "test"
-    }
+    var o = [
+  {
+    "title": "Mein Task",
+    "event": "flame",
+    "moment": "Gerade",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      },
+      {
+        "avatar": "bild2.png",
+        "name": "Markus"
+      }
+    ]
+  },
+  {
+    "title": "Mein Task2",
+    "event": "Checkin",
+    "moment": "Vor 8 Minuten",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "title": "Mein Task3",
+    "event": "Checkout",
+    "moment": "Vor einer Stunde",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "title": "Mein Task",
+    "event": "flame",
+    "moment": "Gerade",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      },
+      {
+        "avatar": "bild2.png",
+        "name": "Markus"
+      }
+    ]
+  },
+  {
+    "title": "Mein Task2",
+    "event": "Checkin",
+    "moment": "Vor 8 Minuten",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "title": "Mein Task3",
+    "event": "Checkout",
+    "moment": "Vor einer Stunde",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "title": "Mein Task",
+    "event": "flame",
+    "moment": "Gerade",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      },
+      {
+        "avatar": "bild2.png",
+        "name": "Markus"
+      }
+    ]
+  },
+  {
+    "title": "Mein Task2",
+    "event": "Checkin",
+    "moment": "Vor 8 Minuten",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "title": "Mein Task3",
+    "event": "Checkout",
+    "moment": "Vor einer Stunde",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  }
+];
+
+
+
+app.get('/tasks', function(req, res) {
   res.json(o);
 });
 
