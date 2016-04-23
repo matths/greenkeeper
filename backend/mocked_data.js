b466ae53aa3ac1aa0776af289c51b4bbdc1c2dd1
Matthias Dittgen <matthias.dittgen@valiton.com>
2016-04-23 18:11:48 +0200

    Update backend

---
+++ backend/mocked_data.js
@@ -0,0 +1,169 @@
+var _stream = [
+  {
+    "id": "a6f0ce02-b4e0-5eee-4db1-f96f59e7ddeb",
+    "title": "Mein Task",
+    "event": "flame",
+    "moment": "Gerade",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      },
+      {
+        "avatar": "bild2.png",
+        "name": "Markus"
+      }
+    ]
+  },
+  {
+    "id": "fa5afe09-5eb5-f526-d7a6-e0a840977f79",
+    "title": "Mein Task2",
+    "event": "Checkin",
+    "moment": "Vor 8 Minuten",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "id": "76530a73-9d09-baa0-4a74-763f7a6c9fb6",
+    "title": "Mein Task3",
+    "event": "Checkout",
+    "moment": "Vor einer Stunde",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "id": "d85a0b21-af0f-6a44-ae7c-472354098846",
+    "title": "Mein Task",
+    "event": "flame",
+    "moment": "Gerade",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      },
+      {
+        "avatar": "bild2.png",
+        "name": "Markus"
+      }
+    ]
+  },
+  {
+    "id": "dbf63927-f7cf-3f32-a32b-ce8dc36c06a4",
+    "title": "Mein Task2",
+    "event": "Checkin",
+    "moment": "Vor 8 Minuten",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "id": "c746805a-4cc9-0b84-1909-eaca600c76bf",
+    "title": "Mein Task3",
+    "event": "Checkout",
+    "moment": "Vor einer Stunde",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "id": "ab932862-e69b-0a17-8d9c-5595da027daf",
+    "title": "Mein Task",
+    "event": "flame",
+    "moment": "Gerade",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      },
+      {
+        "avatar": "bild2.png",
+        "name": "Markus"
+      }
+    ]
+  },
+  {
+    "id": "2e87b74d-7539-56b1-30dd-637d15d33ec1",
+    "title": "Mein Task2",
+    "event": "Checkin",
+    "moment": "Vor 8 Minuten",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  },
+  {
+    "id": "3cb11174-4e6b-102c-a1bb-13b8aa0c0301",
+    "title": "Mein Task3",
+    "event": "Checkout",
+    "moment": "Vor einer Stunde",
+    "description": "Description Description Description Description",
+    "users": [
+      {
+        "avatar": "bild1.png",
+        "name": "Matthias"
+      }
+    ]
+  }
+];
+
+var _tasks = [
+  {
+    "id": "baf209a4-1de4-b48d-791c-46d92182041a",
+    title: "Tür anschrauben",
+    description: "idsihvdsiov",
+    project: "Waschmaschine",
+    client: "AEG"
+  },
+  {
+    "id": "0d39c98b-9cbf-786c-0eb5-099ce685f8d6",
+    title: "Motor anschliessen",
+    description: "idsihvdsiov",
+    project: "E-Fahrrad",
+    client: "Bosch"
+  },
+  {
+    "id": "fb5e630d-8f6d-8123-90f3-0093759ade58",
+    title: "Pedale anbringen",
+    description: "idsihvdsiov",
+    project: "E-Fahrrad",
+    client: "Bosch"
+  }
+];
+
+var _users = [
+  {
+    id: "cb5847ff-ee2b-e860-7bdd-e6ce96481e74",
+    avatar: "bild1.png",
+    name: "Matthias"
+  }
+];
+
+module.exports = {
+  'tasks': _tasks,
+  'stream': _stream,
+  'users': _users
+};
