cfbf524578856003f43a9c1e0c9bb30b9a67e19c
Matthias Dittgen <matthias.dittgen@valiton.com>
2016-04-23 20:59:27 +0200

    new mock data and api endpoints

--- backend/mocked_data.js
+++ backend/mocked_data.js
@@ -1,130 +1,147 @@
 var _stream = [
   {
     "id": "a6f0ce02-b4e0-5eee-4db1-f96f59e7ddeb",
-    "title": "Mein Task",
+    "title": "Neue Sommerserie: Die schönsten Badeseen",
     "event": "flame",
     "moment": "Gerade",
-    "description": "Description Description Description Description",
+    "description": "Multimediales Storytelling: Interaktive Karte, Videos und Bildergalerien zu Badeseen im Landkreis. Grafik fehlt!",
     "users": [
       {
-        "avatar": "bild1.png",
+        "id": "na",
+        "avatar": "matthias.png",
         "name": "Matthias"
       },
       {
-        "avatar": "bild2.png",
+        "avatar": "markus.png",
         "name": "Markus"
       }
     ]
   },
   {
     "id": "fa5afe09-5eb5-f526-d7a6-e0a840977f79",
-    "title": "Mein Task2",
+    "title": "Integration von Pageflow in den Webserver",
     "event": "Checkin",
     "moment": "Vor 8 Minuten",
-    "description": "Description Description Description Description",
+    "description": "Zusammen mit dem Online:-Team Pageflow installieren, anpassen und testen",
     "users": [
       {
-        "avatar": "bild1.png",
-        "name": "Matthias"
+        "id": "na",
+        "avatar": "tobias.png",
+        "name": "Tobias"
+        },
+      {
+        "avatar": "jan.png",
+        "name": "Jan"
+        },
+      {
+        "avatar": "ricarda.png",
+        "name": "Ricarda"
       }
     ]
   },
   {
     "id": "76530a73-9d09-baa0-4a74-763f7a6c9fb6",
-    "title": "Mein Task3",
+    "title": "Wochenend-Termine planen",
     "event": "Checkout",
     "moment": "Vor einer Stunde",
-    "description": "Description Description Description Description",
+    "description": "Vorbereiten der Montagsausgabe",
     "users": [
       {
-        "avatar": "bild1.png",
-        "name": "Matthias"
+        "id": "na",
+        "avatar": "juergen.png",
+        "name": "Jürgen"
       }
     ]
   },
   {
     "id": "d85a0b21-af0f-6a44-ae7c-472354098846",
-    "title": "Mein Task",
+    "title": "Luftaufnahmen mit der Drohne",
     "event": "flame",
     "moment": "Gerade",
-    "description": "Description Description Description Description",
+    "description": "Stadt genehmigt Flug über Rathaus nicht",
     "users": [
       {
-        "avatar": "bild1.png",
-        "name": "Matthias"
+        "id": "na",
+        "avatar": "oliver.png",
+        "name": "Oliver"
       },
       {
-        "avatar": "bild2.png",
-        "name": "Markus"
+        "avatar": "lars.png",
+        "name": "Lars"
       }
     ]
   },
   {
     "id": "dbf63927-f7cf-3f32-a32b-ce8dc36c06a4",
-    "title": "Mein Task2",
+    "title": "Intro für Videos entwickeln",
     "event": "Checkin",
     "moment": "Vor 8 Minuten",
-    "description": "Description Description Description Description",
+    "description": "Neues Intro für Redaktionsvideos basteln - Logo fehlt auch noch",
     "users": [
       {
-        "avatar": "bild1.png",
-        "name": "Matthias"
+        "id": "3456",
+        "avatar": "joerg.png",
+        "name": "Jörg"
       }
     ]
   },
   {
     "id": "c746805a-4cc9-0b84-1909-eaca600c76bf",
-    "title": "Mein Task3",
+    "title": "Frage der Woche schneiden",
     "event": "Checkout",
     "moment": "Vor einer Stunde",
-    "description": "Description Description Description Description",
+    "description": "Umfrage in der Fußgängerzone - Fotos hochladen und Video fertig schneiden",
     "users": [
       {
-        "avatar": "bild1.png",
-        "name": "Matthias"
+        "id": "na",
+        "avatar": "carolin.png",
+        "name": "Carolin"
       }
     ]
   },
   {
     "id": "ab932862-e69b-0a17-8d9c-5595da027daf",
-    "title": "Mein Task",
+    "title": "Wandertag organisieren",
     "event": "flame",
     "moment": "Gerade",
-    "description": "Description Description Description Description",
+    "description": "Sommer-Wandertag mit Lesern vorbereiten - Wanderkarte fehlt, Ankündigung nicht geschrieben, interaktive Karte ist falsch",
     "users": [
       {
-        "avatar": "bild1.png",
-        "name": "Matthias"
+        "id": "na",
+        "avatar": "wolfgang.png",
+        "name": "Wolfgang"
       },
       {
-        "avatar": "bild2.png",
-        "name": "Markus"
+        "avatar": "jessica.png",
+        "name": "Jessica"
       }
     ]
   },
   {
     "id": "2e87b74d-7539-56b1-30dd-637d15d33ec1",
-    "title": "Mein Task2",
+    "title": "Facebook-Benchmark",
     "event": "Checkin",
     "moment": "Vor 8 Minuten",
-    "description": "Description Description Description Description",
+    "description": "Statistik aller Facebook-Auftritte von Zeitungen im Verbreitungsgebiet vorbereiten",
     "users": [
       {
-        "avatar": "bild1.png",
-        "name": "Matthias"
+        "id": "na",
+        "avatar": "simone.png",
+        "name": "Simone"
       }
     ]
   },
   {
     "id": "3cb11174-4e6b-102c-a1bb-13b8aa0c0301",
-    "title": "Mein Task3",
+    "title": "Schulung",
     "event": "Checkout",
     "moment": "Vor einer Stunde",
-    "description": "Description Description Description Description",
+    "description": "Kollegen in neuem Redaktions-Managementsystem schulen",
     "users": [
       {
-        "avatar": "bild1.png",
-        "name": "Matthias"
+        "id": "na",
+        "avatar": "martin.png",
+        "name": "Martin"
       }
     ]
   }
@@ -133,31 +150,31 @@ var _stream = [
 var _tasks = [
   {
     "id": "baf209a4-1de4-b48d-791c-46d92182041a",
-    title: "Tür anschrauben",
-    description: "idsihvdsiov",
-    project: "Waschmaschine",
-    client: "AEG"
+    title: "Drohneneinsatz",
+    description: "Luftaufnahmen von Gemeinde Dorfhausen machen",
+    project: "Gemeindeporträt",
+    client: "Dorfhausen"
   },
   {
     "id": "0d39c98b-9cbf-786c-0eb5-099ce685f8d6",
-    title: "Motor anschliessen",
-    description: "idsihvdsiov",
-    project: "E-Fahrrad",
-    client: "Bosch"
+    title: "Kneipenfestival-Sonderseiten",
+    description: "Acht Seiten zu Jazz in Town",
+    project: "Kneipenfest",
+    client: "Jazz-Verein"
   },
   {
     "id": "fb5e630d-8f6d-8123-90f3-0093759ade58",
-    title: "Pedale anbringen",
-    description: "idsihvdsiov",
-    project: "E-Fahrrad",
-    client: "Bosch"
+    title: "Neuer Workflow Bildergalerien",
+    description: "Entwicklung von einer einfachen Möglichkeit Fotos hochzuladen",
+    project: "Fotos",
+    client: "Redaktion"
   }
 ];
 
 var _users = [
   {
     id: "cb5847ff-ee2b-e860-7bdd-e6ce96481e74",
-    avatar: "bild1.png",
+    avatar: "matthias.png",
     name: "Matthias"
   }
 ];
