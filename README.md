c585e6642dc9215e4e36b7d9aec9eb3cbe135d9d
Matthias Dittgen <matthias.dittgen@valiton.com>
2016-04-23 16:39:10 +0200

    new fake backend

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 
 
+we use stylus and jeet
+
+http://stylus-lang.com/docs/selectors.html
+http://jeet.gs/
+
 compile .styl to .css using the jeet grid system
 
 	stylus -u jeet -w css/styles.styl
--- backend/fakeBackend.js
+++ backend/fakeBackend.js
@@ -1,45 +1,12 @@
-var jswiremocklib, jswiremock, stubFor, get, post, put, urlEqualTo, a_response;
-jswiremocklib = require('jswiremock'),
-    jswiremock = jswiremocklib.jswiremock,
-    stubFor = jswiremocklib.stubFor,
-    get = jswiremocklib.get,
-    post = jswiremocklib.post,
-    // put = jswiremocklib.put, not impl
-    urlEqualTo = jswiremocklib.urlEqualTo, a_response = jswiremocklib.a_response;
+var express = require('express');
+var app = express();
 
-var jswiremock = new jswiremock(5001); //port 
+app.get('/tasks', function(req, res){
+    var o = {
+        avatar: "cdsvs.png",
+        test: "test"
+    }
+  res.json(o);
+});
 
-stubFor(jswiremock, get(urlEqualTo("/actor/:varying_var/"))
-    .willReturn(a_response()
-        .withStatus(200)
-        .withHeader({"Content-Type": "application/json"})
-        .withBody("some Jso fasdfkjj n")));
-
-stubFor(jswiremock, post(urlEqualTo("/rate"), {type: "down", newsid: ":varying_var"})
-    .willReturn(a_response()
-        .withStatus(200)
-        .withStatus(200)
-        .withHeader({"Content-Type": "application/json"})
-        .withBody("[{\"status\":\"success\"}]")));
-
-stubFor(jswiremock, post(urlEqualTo("/rate/:varying_var/down/"))
-    .willReturn(a_response()
-        .withStatus(200)
-        .withHeader({"Content-Type": "application/json"})
-        .withBody("[{\"status\":\"success\"}]")));
-
-
-stubFor(jswiremock, post(urlEqualTo("/rate/:varying_var/up/"))
-    .willReturn(a_response()
-        .withStatus(200)
-        .withHeader({"Content-Type": "application/json"})
-        .withBody("[{\"status\":\"success\"}]")));
-
-stubFor(jswiremock, post(urlEqualTo("/login"), {username: "flo", password: "geheim"})
-    .willReturn(a_response()
-        .withStatus(200)
-        .withHeader({})
-        .withBody("")));
-
-// jswiremock.stopJswiremock();
-
+app.listen(5000);
