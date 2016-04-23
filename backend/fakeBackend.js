6ba3c59fb5054152fa7f94429c5f5324fe5828ea
bs123 <dojo@nospam.tk>
2016-04-23 16:47:45 +0200

    cors, fakebackend

--- backend/fakeBackend.js
+++ backend/fakeBackend.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+ app.use('/*', function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        next();
+    });
+
 app.get('/tasks', function(req, res){
     var o = {
         avatar: "cdsvs.png",
