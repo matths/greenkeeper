fdb774265a3d6516f07f2a934ea6b0cde8fac7e0
Matthias Dittgen <matthias.dittgen@valiton.com>
2016-04-23 15:00:28 +0200

    update

--- js/main.js
+++ js/main.js
@@ -9,9 +9,19 @@ xtag.register('x-avatar', {
 });
 
 xtag.register('x-task', {
+	content: '<h2></h2>',
 	lifecycle: {
 		created: function () {
 			this.xtag.avatar = this.querySelector('x-avatar');
+			this.xtag.title = this.querySelector('h2');
+			this.xtag.title.textContent = this.title;
+		}
+	},
+	accessors: {
+		title: {
+			set: function (value) {
+        	    this.xtag.title.textContent = value;
+        	}
 		}
 	},
 	methods: {
@@ -19,7 +29,6 @@ xtag.register('x-task', {
 			var self = this;
             $.each(users, function(key, value) {
 	            var el = $('<x-avatar></x-avatar>');
-	            el.attr('title', value.title);
 	            el.get(0).xtag.img.src = value.avatar;
                 $(self).append(el);
 			});
@@ -30,18 +39,39 @@ xtag.register('x-task', {
 xtag.register('x-latesttasks', {
 	lifecycle: {
 		created: function () {
-            this.fetch();
+		}
+	},
+});
+
+xtag.register('x-taskloop', {
+	lifecycle: {
+		created: function () {
+		}
+	},
+});
+
+xtag.register('x-app', {
+	lifecycle: {
+		created: function () {
+            this.xtag.latesttasks = this.querySelector('x-latesttasks');
+            this.xtag.taskloop = this.querySelector('x-taskloop');
+            this.initialFetch();
 		}
 	},
     methods: {
-        fetch: function () {
+        initialFetch: function () {
         	var self = this;
             $.getJSON("http://127.0.0.1:8000/api.json", function (data) {
                 $.each(data, function(key, value) {
                     var el = $('<x-task></x-task>');
                     el.attr('title', value.title);
                     el.get(0).buildUsers(value.users);
-                    $(self).append(el);
+
+                    if (value.event == "flame") {
+                	    $(self.xtag.latesttasks).append(el);
+                	} else {
+            		    $(self.xtag.taskloop).append(el);
+            		}
                 });
             });
 
